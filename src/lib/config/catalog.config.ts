361e1c8cea74adaa425d90c4a03e7b16a5686e10
LadislaoAlvarez16 <167925972+LadislaoAlvarez16@users.noreply.github.com>
2026-02-05 21:54:53 -0300

    feat: configuracion del modal dependiendo el tipo de plan activo

--- src/lib/config/catalog.config.ts
+++ src/lib/config/catalog.config.ts
@@ -1,7 +1,9 @@
 import type { Plan } from "@/lib/plan/plan.config";
 
 export const CATALOG_CONFIG = {
-    plan: "basic" as Plan,
+    plan: "medium" as Plan,
     whatsapp: "5491112345678",
     whatsappMessage: "Hola, quiero consultar por este producto",
 };
+
+//PLANE: "basic, medium, pro."
