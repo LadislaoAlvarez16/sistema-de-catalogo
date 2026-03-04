81b9aefdc95227bcdf7c4cf456557ec7e4e9906e
LadislaoAlvarez16 <167925972+LadislaoAlvarez16@users.noreply.github.com>
2026-03-03 21:26:51 -0300

    feat: implementar acceder con el plan pro al modal del producto

--- src/components/catalog/ProductModal.tsx
+++ src/components/catalog/ProductModal.tsx
@@ -4,38 +4,34 @@ import type { Product } from "@/types/product";
 import Image from "next/image";
 import Link from "next/link";
 import { getProductImageUrl } from "@/lib/storage/getProductImageUrl";
-import { getCatalogConfig } from "@/lib/config/getCatalogConfig";
+import type { Plan } from "@/lib/plan/plan.config";
 import { canUseProductPage } from "@/lib/plan/plan.helpers";
-
 
 type Props = {
     product: Product;
+    plan: Plan; // Recibir plan como prop para determinar funcionalidades dentro del modal
     onClose: () => void;
 };
 
-export default function ProductModal({ product, onClose }: Props) {
+export default function ProductModal({ product, plan, onClose }: Props) {
     const imageSrc = getProductImageUrl(product.image_url);
-    const { plan } = getCatalogConfig();
-    const canOpenProductPage = canUseProductPage(plan);
+    const canOpenProductPage = canUseProductPage(plan); //Usar plan recibido por props
 
     return (
         <div
             className="fixed inset-0 z-50 flex items-center justify-center bg-black/70"
             onClick={onClose}
         >
-
             <div
                 className="relative w-full max-w-lg rounded-xl bg-zinc-950 p-6 border border-zinc-800"
                 onClick={(e) => e.stopPropagation()}
             >
-
                 <button
                     onClick={onClose}
                     className="absolute top-3 right-3 text-zinc-400 hover:text-white"
                 >
                     ✕
                 </button>
-
                 <Image
                     src={imageSrc}
                     alt={product.name}
@@ -43,16 +39,11 @@ export default function ProductModal({ product, onClose }: Props) {
                     height={500}
                     className="w-full h-64 object-cover rounded-lg mb-4"
                 />
-
-
                 <h2 className="text-xl font-semibold mb-2">{product.name}</h2>
-
                 {product.description && (
                     <p className="text-zinc-400 mb-3">{product.description}</p>
                 )}
-
                 <p className="text-sm text-zinc-500">Categoría: {product.category} </p>
-
                 {canOpenProductPage && (
                     <Link
                         href={`/product/${product.slug}`}
